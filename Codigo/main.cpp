65cf4faeb36cce6e8dcd262bb0b9aded9245004e
javier <33096723+javi20gu@users.noreply.github.com>
2018-05-26 19:01:52 +0200

    Añadido Archivos Restantes
    Versión Estable

---
+++ Codigo/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+void obtenerBeneficio(float cantFija, float cantVariable, float precioventa, float cantproductos){
+		
+	int resultado = 0;
+		
+	resultado = (precioventa - cantVariable) * cantproductos - cantFija;
+	cout<<"\nTu Beneficio va a ser de: " << resultado <<" $. Por vender "<<cantproductos<<" uds."<<endl;
+}
+
+void obtenerUnidades(float cantFija, float cantVariable, float precioventa){
+	
+	int resultado = 0;
+	
+	resultado = cantFija / (precioventa - cantVariable);
+	cout<<"Tienes que vender " << resultado << "uds. Para tener un beneficio 0";
+	
+}
+
+int main(){
+	
+	float cantidadFijaUnitaria, cantidadVariableUnitaria, precioVentaOrdenador, cantidaddeproductos;
+	
+	cout<<"Indique la cantidad de productos si quiere obtener el beneficio sino ponga 0: ";
+	cin>>cantidaddeproductos;
+	
+	cout<<"Indique la cantidad fija unitaria: ";
+	cin>>cantidadFijaUnitaria;
+	
+	cout<<"Indique la cantidad variable unitaria: ";
+	cin>>cantidadVariableUnitaria;
+	
+	cout<<"Indique el precio al público del producto: ";
+	cin>>precioVentaOrdenador;
+	
+	if (cantidaddeproductos == 0 ){
+		obtenerUnidades(cantidadFijaUnitaria, cantidadVariableUnitaria, precioVentaOrdenador);
+	} 
+	else {
+		obtenerBeneficio(cantidadFijaUnitaria, cantidadVariableUnitaria, precioVentaOrdenador, cantidaddeproductos);
+	}
+	getch();
+	return 0;
+}
